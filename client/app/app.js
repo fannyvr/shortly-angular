bd6aa4adef1ec3e1f129a030490ffa27dcb37c4c
Fanny Vasquez <fannyvr18@gmail.com>
2016-02-12 17:18:11 -0800

    "added route views"

--- client/app/app.js
+++ client/app/app.js
@@ -15,7 +15,19 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
-    // Your code here
+    // ******Your code here*******
+    .when('/links', {
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
+    })
+    .when('/shorten',{
+      templateUrl: 'app/shorten/shorten.html',
+      controller: 'ShortenController'
+    })
+    .otherwise({
+      redirectTo: '/signin'
+    })
+
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
